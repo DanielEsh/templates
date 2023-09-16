215f95d07dae47f09798c142a862529f0a9469cd
Daniel <daniilterawars@gmail.com>
2023-09-16 21:36:30 +0300

    feat: env port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
+
+const DEFAULT_PORT = 3000;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(async ({mode}) => {
+  const { PORT } = loadEnv(mode, process.cwd(), '');
+
+  console.log('PORT', )
+  return {
+    plugins: [vue()],
+    server: {
+      port: +PORT || DEFAULT_PORT,
+    }
+  }
 })

1928ba009e8024c6f8b933fbfcaf192d30415459
Daniel <daniilterawars@gmail.com>
2023-09-18 21:36:45 +0300

    feat(router): types

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -1,30 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [
+export enum AppLayouts {
+  DEFAULT = 'DefaultLayout',
+}
+
+export enum AppRoutes {
+  HOME = 'home',
+  ABOUT = 'about',
+}
+
+export const RoutePath: Record<AppRoutes, string> = {
+  [AppRoutes.HOME]: '/',
+  [AppRoutes.ABOUT]: '/about',
+}
+
+interface AppRouter extends Omit<RouteRecordRaw, 'name' | 'children'> {
+  name: AppLayouts | AppRoutes
+  children?: AppRouter[]
+}
+
+const routes: AppRouter[] = [
   {
-    path: '/',
-    name: 'DefaultLayout',
+    path: RoutePath.home,
+    name: AppLayouts.DEFAULT,
     component: () => import('../../layouts/app-layout.vue'),
     children: [
       {
         path: '',
+        name: AppRoutes.HOME,
         component: () => import('../../pages/index-page.vue'),
       },
     ],
   },
   {
-    path: '/test',
-    children: [
-      {
-        path: '',
-        component: () => import('../../pages/index-page.vue'),
-      },
-    ],
+    path: '/about',
+    name: AppRoutes.ABOUT,
+    component: () => import('../../pages/about-page.vue'),
   },
 ]
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: routes as RouteRecordRaw[],
 })

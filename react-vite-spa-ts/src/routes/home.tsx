9d5449e52fccbd31ff612b54f1df64e6441018b4
Daniel <daniilterawars@gmail.com>
2024-03-27 22:08:34 +0300

    react: env support

--- react-vite-spa-ts/src/routes/home.tsx
+++ react-vite-spa-ts/src/routes/home.tsx
@@ -1,9 +1,11 @@
 import {Link} from "react-router-dom";
 
 export default function HomePage() {
+
     return (
         <div>
             <h1>Home Page</h1>
+            {String(import.meta.env.VITE_ENV_VARIABLE)}
             <div>
                 <Link to="/categories">To categories</Link>
             </div>
--- react-vite-spa-ts/vite.config.ts
+++ react-vite-spa-ts/vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath, URL } from 'url'
+import { defineConfig, loadEnv } from 'vite'
+
+const DEFAULT_PORT = 5173
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
+export default defineConfig(async ({ mode }) => {
+  const { PORT } = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+    ],
+    server: {
+      port: +PORT || DEFAULT_PORT,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
   }
 })
